5fc5db5bdee9aaa6daed4b446ecac24b0f014871
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-02-14 17:35:04 -0500

    added intro from pizzaz

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Charlotte" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Charlotte Woodrum and I’m a junior from Atlanta, Georgia. I’m a hackathon organizer, FRC programmer, computer science tutor, cross-country runner, and book enjoyer (mostly fiction) exploring AI and software development. I’m excited to meet you all and explore new areas of computer science!")
 
 if __name__ == "__main__": # checks if current script being run as a main program, or if as a module (part of another program)
     main()

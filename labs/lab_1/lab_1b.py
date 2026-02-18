857b2e25a5a3412aee89795e7d3132e48ae8fc24
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-02-18 08:23:31 -0500

    added operation checks

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,19 +37,35 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(message): 
+    while True: 
+        try: 
+            num = float(input(message))
+            return float(num)
+        except ValueError: 
+            print("Please enter a number")
+
+def request_valid_operation(): 
+    operation = ""
+    while True: 
+        operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+        if operation in ["add", "subtract", "multiply", "divide"]: 
+            return operation
+        print("Please enter a valid operation (add, subtract, multiply, or divide)")
+        
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = request_sanitized_number("Enter the first number: ")
+    num2 = request_sanitized_number("Enter the second number: ")
+    operation = request_valid_operation()
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
     print(f"The result of {operation}ing {num1} and {num2} is: {result}")
-
 
 if __name__ == "__main__":
     main()
